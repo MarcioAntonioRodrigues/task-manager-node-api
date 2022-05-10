f269024d1763e926d18dd7616628d88965973448
marcio <ciorma7@hotmail.com>
2022-05-10 16:07:19 -0300

    Changing script of creation of tasks table

--- sql/create_task_table.sql
+++ sql/create_task_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE task
 (
     taskId AUTOINCREMENT NOT NULL,
     name VARCHAR(50) NOT NULL,
-    timeCourse VARCHAR (30) NOT NULL,
     priority VARCHAR(20) NOT NULL,
     status VARCHAR(20) NOT NULL,
     description VARCHAR(255) NULL,
